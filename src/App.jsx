42fd0c8c9ff91a6148b36e47ce8da587735befdf
minarkar22 <arkarminnaing98@gmail.com>
2025-02-05 23:29:41 +0300

    UI Layout

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Header from './component/Header'
+import Balance from './component/Balance'
+import ExpenseIncome from './component/ExpenseIncome'
+import TranscationList from './component/TranscationList'
+import AddTranscation from './component/AddTranscation'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      <Balance/>
+      <ExpenseIncome/>
+      <TranscationList/>
+      <AddTranscation/>
+    </>
+  )
+}
+
+export default App
