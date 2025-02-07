a79d327a641a0b3cdd504dd5a27cafa9a1134fca
minarkar22 <arkarminnaing98@gmail.com>
2025-02-07 12:14:12 +0300

    Add delete feature

--- src/component/ExpenseIncome.jsx
+++ src/component/ExpenseIncome.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 function ExpenseIncome() {
+  const { transactions } = useContext(GlobalContext); 
+  const amounts = transactions.map(transaction => transaction.amount );
+  const income = amounts
+              .filter(item => item > 0)
+              .reduce((acc, item) => (acc += item), 0)
+              .toFixed(2);
+  // console.log('Income ', income);
+
+  const expense = amounts
+              .filter(item => item < 0)
+              .reduce((acc, item) => (acc += item), 0)
+              .toFixed(2) * -1;
+  // console.log("expense ", expense);
+  
   return (
     <div className="inc-exp-container">
     <div>
       <h4>Income</h4>
-      <p className="money plus">+$0.00</p>
+      <p className="money plus">+${ income }</p>
     </div>
     <div>
       <h4>Expense</h4>
-      <p className="money minus">-$0.00</p>
+      <p className="money minus">-${ expense }</p>
     </div>
   </div>
   )
--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -7,10 +7,16 @@ import AppReducer from "./AppReducer";
 // Initial state
 const InitialState = {
     transactions : [
-  { id: 1, text: 'Flower', amount: -20 },
-  { id: 2, text: 'Salary', amount: 300 },
-  { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: 150 }
+        { id: 1, text: 'Flower', amount: -20 },
+        { id: 2, text: 'Salary', amount: 300 },
+        { id: 3, text: 'Book', amount: -10 },
+        { id: 4, text: 'Camera', amount: 150 },
+        { id: 5, text: 'Groceries', amount: -50 },
+        { id: 6, text: 'Freelance Work', amount: 200 },
+        { id: 7, text: 'Dinner', amount: -30 },
+        { id: 8, text: 'Bonus', amount: 100 },
+        { id: 9, text: 'Movie Tickets', amount: -25 },
+        { id: 10, text: 'Investment Dividends', amount: 75 }
 ]}
   
 //Create Context
@@ -20,9 +26,18 @@ export const GlobalContext = createContext(InitialState);
 export default function GlobalProvider ({ children }) {
     const [state, dispatch] = useReducer(AppReducer, InitialState);
 
+    //Action and Payload
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
     return (
         <GlobalContext.Provider value={{
-            transactions: state.transactions
+            transactions: state.transactions,
+            deleteTransaction
         }}>
             {children}
         </GlobalContext.Provider>
